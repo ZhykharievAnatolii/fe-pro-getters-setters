55dba010f878778f2907e9374e90cbf08149863d
ZhykharievAnatolii <nekijt87@gmail.com>
2022-11-02 21:29:19 +0200

    upd js file
    another version

--- src/index.js
+++ src/index.js
@@ -8,40 +8,53 @@ function Student(name, grades) {
     },
   });
 }
-export const school = {
-      students: {
-        0: new Student('Maria', [45, 76, 45, 98, 90, 87, 56]),
-        1: new Student('Dmytro', [67, 34, 87, 34, 98, 76]),
-        2: new Student('Anton', [45, 34, 87, 34, 23, 76]),
-        3: new Student('Olha', [67, 34, 34, 23, 98, 76]),
-        4: new Student('Anna', [67, 34, 85, 34, 85, 76]),
-        5: new Student('Bohdan', [67, 25, 87, 34, 25, 76]),
-        6: new Student('Eugene', [97, 34, 78, 85, 98, 65]),
-        7: new Student('Ivan', [76, 89, 78, 98, 98, 99, 89, 96]),
-      },
-      get aGradeStudents(){
-        return Object.values(this.students).filter((student)=>{
-          return student.averageGrade>=90&&student.averageGrade<=100
-        }).map((item)=>{
-          return item.name}).join(', ')
-      },
-      get bGradeStudents(){
-        return Object.values(this.students).filter((student)=>{
-          return student.averageGrade>=75&&student.averageGrade<=89
-        }).join(', ')
-      },
-      get cGradeStudents()
-      {
-        return Object.values(this.students).filter((student)=>{
-          return student.averageGrade>=60&&student.averageGrade<=75
-        }).join(', ')
-      },
-      get dGradeStudents(){
-        return Object.values(this.students).filter((student)=>student.averageGrade>=0&&student.averageGrade<=59).join(', ')
-      },
-    }
-;
 
+export const school = {
+  students: {
+    0: new Student('Maria', [45, 76, 45, 98, 90, 87, 56]),
+    1: new Student('Dmytro', [67, 34, 87, 34, 98, 76]),
+    2: new Student('Anton', [45, 34, 87, 34, 23, 76]),
+    3: new Student('Olha', [67, 34, 34, 23, 98, 76]),
+    4: new Student('Anna', [67, 34, 85, 34, 85, 76]),
+    5: new Student('Bohdan', [67, 25, 87, 34, 25, 76]),
+    6: new Student('Eugene', [97, 34, 78, 85, 98, 65]),
+    7: new Student('Ivan', [76, 89, 78, 98, 98, 99, 89, 96]),
+  },
+
+  get aGradeStudents() {
+    return Object.values(this.students)
+        .filter((student) => student.averageGrade >= 90)
+        .map((student) => student.name)
+        .join(', ');
+  },
+
+  get bGradeStudents() {
+    return Object.values(this.students)
+        .filter(
+            (student) => student.averageGrade >= 75 && student.averageGrade <= 89
+        )
+        .map((student) => student.name)
+        .join(', ');
+  },
+
+  get cGradeStudents() {
+    return Object.values(this.students)
+        .filter(
+            (student) => student.averageGrade >= 60 && student.averageGrade < 75
+        )
+        .map((student) => student.name)
+        .join(', ');
+  },
+
+  get dGradeStudents() {
+    return Object.values(this.students)
+        .filter(
+            (student) => student.averageGrade >= 0 && student.averageGrade <= 59
+        )
+        .map((student) => student.name)
+        .join(', ');
+  },
+};
 // У нас есть объект school и нужно добавить ему 3 геттера.
 //     aGradeStudents который будет возвращать имена студентов перечисленных через запятую в виде строки, которые имеют балл
 // от 100 до 90 включительно. Например, 'John, Mike'
